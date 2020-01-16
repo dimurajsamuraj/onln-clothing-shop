1faafa7bb8f46bce866cf29defb7b56aa3a78edf
dimurajsamuraj <dimurajsamuraj@gmail.com>
2020-01-16 17:12:43 +0100

    pulling shop data from firestore and storing into reducer from shop page. And included withSpinner HOC

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,7 +11,9 @@ const config = {
     messagingSenderId: "1002196449927",
     appId: "1:1002196449927:web:d9a45c8c8227d5010b9622",
     measurementId: "G-0BHJEPN1JK"
-  }
+  };
+
+  firebase.initializeApp(config);
 
   export const createUserProfileDocument = async (userAuth, additionalData) => {
     if (!userAuth) return;
@@ -37,9 +39,37 @@ const config = {
     }
 
     return userRef;
-  }
+  };
 
-  firebase.initializeApp(config);
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
+    
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+      const newDocRef = collectionRef.doc();
+      batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();
+  };
+
+  export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+      const { title, items } = doc.data();
+
+      return {
+        routeName: encodeURI(title.toLowerCase()),
+        id: doc.id,
+        title,
+        items
+      }
+    });
+
+    return transformedCollection.reduce((accumulator, collection) => {
+      accumulator[collection.title.toLowerCase()] = collection;
+      return accumulator;
+    }, {});
+  };
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
