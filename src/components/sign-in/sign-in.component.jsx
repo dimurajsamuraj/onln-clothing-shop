83ab890c1d90b42f4e4837fb27e62597c4dc8169
dimurajsamuraj <dimurajsamuraj@gmail.com>
2020-01-26 10:39:51 +0100

    updated App component, Sign-in and Sign-up component and Shop page to use hooks

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from "react-redux";
 
 import FormInput from '../form-input/form-input.component';
@@ -8,41 +8,33 @@ import { googleSignInStart, emailSignInStart } from "../../redux/user/user.actio
 
 import './sign-in.styles.scss';
 
-class SignIn extends React.Component {
-    constructor(props) {
-        super(props);
+const SignIn = ({ emailSignInStart, googleSignInStart }) => {
+    const [userCredentials, setUserCredentials] = useState({ email: '', password: '' });
+    
+    const { email, password } = userCredentials;
 
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
-
-    handleSubmit = async event => {
+    const handleSubmit = async event => {
         event.preventDefault();
-
-        const { emailSignInStart } = this.props;
-        const { email, password } = this.state;
 
         emailSignInStart(email, password);
     }
 
-    handleChange = event => {
+    const handleChange = event => {
         const { value, name } = event.target;
 
-        this.setState({ [name]: value })
+        setUserCredentials({ ...userCredentials, [name]: value })
     }
 
-    render() {
-        const { googleSignInStart } = this.props;
         return(
             <div className='sign-in'>
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput type="email" name="email" handleChange={this.handleChange} label='Email' value={this.state.email} required />
-                    <FormInput type="password" name="password" handleChange={this.handleChange} label='Password' value={this.state.password} required />
+                <form onSubmit={handleSubmit}>
+                    <FormInput type="email" name="email" handleChange={handleChange} label='Email' value={email} required />
+
+                    <FormInput type="password" name="password" handleChange={handleChange} label='Password' value={password} required />
+
                     <div className='buttons'>
                         <CustomButton type="submit">Sign in</CustomButton>
                         <CustomButton type="button" onClick={googleSignInStart} isGoogleSignIn>Sign in with Google</CustomButton>
@@ -51,7 +43,7 @@ class SignIn extends React.Component {
             </div>
         )
     }
-}
+
 
 const mapDispatchToProps = (dispatch) => ({
     googleSignInStart: () => dispatch(googleSignInStart()),
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -7,14 +7,10 @@ import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 import CollectionOverviewContainer from '../../components/collection-overview/collection-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
 
-class ShopPage extends React.Component {
-    componentDidMount() {
-        const { fetchCollectionsStart } = this.props;
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+    useEffect(() => {
         fetchCollectionsStart();
-    }
-    
-    render() {
-        const { match } = this.props;
+    }, [fetchCollectionsStart]);
 
         return (
             <div className='shop-page'>
@@ -29,7 +25,6 @@ class ShopPage extends React.Component {
             </div>
         );
     }
-}
 
 const mapDispatchToProps = (dispatch) => ({
     fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
